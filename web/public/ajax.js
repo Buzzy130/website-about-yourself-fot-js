7a277586e5f5186853d16c03239227f7cf97ac69
Dmitry <150774391+Buzzy130@users.noreply.github.com>
2024-05-27 00:29:15 +0700

    Add files via upload

---
+++ web/public/ajax.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+    // Ваш код для обработки формы здесь
+});
+</script>
