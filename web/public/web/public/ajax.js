cde55a6343fb30a8104f336d3f7932d22655b8eb
Dmitry <150774391+Buzzy130@users.noreply.github.com>
2024-05-27 00:39:17 +0700

    Delete web/public/web directory

--- web/public/web/public/ajax.js
+++
@@ -1,6 +0,0 @@
-<script>
-    document.getElementById('myForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-    // Ваш код для обработки формы здесь
-});
-</script>
